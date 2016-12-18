e20408077617f8fbcbf7f1f2500e700c639f4d27
林川淇 <charles@linchuanqideMacBook-Pro.local>
2016-12-18 21:06:29 +0800

    edit

--- pages/ui/ui.js
+++ pages/ui/ui.js
@@ -1,5 +1,17 @@
 Page({
   onLoad: function(){
     console.info("ui onLoad")
-  }
+  },
+
+  toCSS1: function() {
+    wx.navigateTo({
+      url: './css1/css1'
+    })
+  },
+
+  toCSS6: function() {
+    wx.navigateTo({
+      url: './css6/css6'
+    })
+  },
 })
