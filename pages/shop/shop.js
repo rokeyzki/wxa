2db0b4fe1f9eb0cca9a5c0e98650dca9022a031d
rokeyzki <rokeyzki@gmail.com>
2016-12-21 17:42:07 +0800

    wxa

---
+++ pages/shop/shop.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      'http://fakeimg.pl/420x252/', // (420/20) * 12 = 252
+      'http://fakeimg.pl/420x252/',
+      'http://fakeimg.pl/420x252/'
+    ],
+    indicatorDots: true,
+    autoplay: true,
+    interval: 3000,
+    duration: 1000
+  },
+});
