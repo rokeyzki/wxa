306f46e070af1b2a074677af09c86edeb9cabca5
rokeyzki <rokeyzki@gmail.com>
2016-12-22 11:48:37 +0800

    new

--- pages/shop/shop.js
+++ pages/shop/shop.js
@@ -10,4 +10,12 @@ Page({
     interval: 3000,
     duration: 1000
   },
+
+  onShareAppMessage: function () {
+    return {
+      title: '自定义分享标题',
+      desc: '自定义分享描述',
+      path: '/page/user?id=123'
+    }
+  }
 });
